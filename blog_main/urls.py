24d246f60c9b787022da68afb6a41c2fb8a13cb5
Hsieh <max870701@gmail.com>
2024-01-09 18:46:23 -0800

    Complete About model and Social Link model

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from blogs import views as BlogsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # home
     path('', views.home, name='home'),
-    path('category/', include('blogs.urls'))
+    path('category/', include('blogs.urls')),
+    path('<slug:blog_slug>/', BlogsView.blogs, name="blogs"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,5 +1,17 @@
 from .models import Category
+from marketing.models import SocialLink, About
 
 def get_categories(request):
     categories = Category.objects.all()
     return dict(categories=categories)
+
+def get_social_links(request):
+    social_links = SocialLink.objects.all()
+    return dict(social_links=social_links)
+
+def get_about(request):
+    try:
+        about = About.objects.get(pk=1)
+        return dict(about=about)
+    except:
+        return dict(about=None)
