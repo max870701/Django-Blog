4809cbc61585b1692d0933ab7e40d9e5449e2236
Hsieh <max870701@gmail.com>
2024-01-18 21:24:59 -0800

    Complete Blog Post CRUD Functionality

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,8 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
+    # Category CRUD
     path('categories/', views.categories, name="categories"),
     path('categories/add', views.add_category, name="add_category"),
     path('categories/edit/<int:pk>/', views.edit_category, name="edit_category"),
     path('categories/delete/<int:pk>/', views.delete_category, name="delete_category"),
+    # Post CRUD
+    path('posts/', views.posts, name="posts"),
+    path('posts/add', views.add_post, name="add_post"),
+    path('post/edit/<int:pk>', views.edit_post, name="edit_post"),
+    path('post/delete/<int:pk>', views.delete_post, name="delete_post"),
 ]
